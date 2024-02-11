f4c8c6037731dead4fa43dabe856100bea517415
SiL3NC3 <sil3nc3@soulwerx.de>
2024-02-11 18:12:09 +0100

    Updates.

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +15,11 @@ namespace MusicSorter
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            // For screenshot purposes only!
+            //Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+        }
     }
 }
